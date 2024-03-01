c711724b381f20c8ae044dffc3d84378f9128349
Noble Udechukwu <nobleude@gmail.com>
2024-03-01 07:47:37 +0000

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Noble Udechukwu on 01/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
